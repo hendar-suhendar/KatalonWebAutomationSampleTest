fee3d96384f5509814f6a671e1188c2c600c291f
hendar-suhendar <myns.suhendar@gmail.com>
2024-01-19 02:41:17 +0700

    Update Formula and Sample HTML Report Before Running Test

--- Scripts/Positive/TC1_Positive_Rekomendasi Harga/Script1705560157034.groovy
+++ Scripts/Positive/TC1_Positive_Rekomendasi Harga/Script1705560157034.groovy
@@ -14,7 +14,9 @@ import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-import internal.GlobalVariable as GlobalVariable
+import internal.GlobalVariable
+import io.netty.handler.codec.redis.DefaultBulkStringRedisContent
+
 import org.openqa.selenium.Keys as Keys
 
 'Buka Web BTN Property'
@@ -40,17 +42,24 @@ WebUI.click(findTestObject("Object Repository/Page_Hitung Harga/button_Hitung"))
 'Vaerifikasi Hasil berasil menampilkan rekomendasi harga yang sesuai dengan perhitungan formula'
 WebUI.verifyElementPresent(findTestObject('Object Repository/Page_Hitung Harga/Title_Harga Properti Maksimal'), 0)
 
-/* Formula yang digunakan saat ini */
-String value = Jangka_Waktu.replaceAll("[^0-9]", "")
-def bil_Penghasilan_Total = Double.parseDouble(Penghasilan_Total)
+/* Verifikasi menggunakan Formula yang digunakan saat ini yaitu :
+ * (Penghasilan - Pengeluaran) * (Jumlah Bulan * Jangka Waktu) : 3*/
+def bil_Penghasilan = Double.parseDouble(Penghasilan_Total)
 def bil_Pengeluaran = Double.parseDouble(Pengeluaran)
-def bil = Integer.parseInt(value)
-def hasil = bil_Penghasilan_Total - bil_Pengeluaran
-def bil_hasil = hasil * bil * 4
-println(bil_hasil)
+
+String number_only_JW = Jangka_Waktu.replaceAll("[^0-9]", "")
+def bil_Jangka_Waktu = Integer.parseInt(number_only_JW)
+
+def bil_sisa_pendapatan = bil_Penghasilan - bil_Pengeluaran
+def bil_Hasil_Jangka_Waktu = 12 * bil_Jangka_Waktu
+def bil_Rekom_Harga = bil_sisa_pendapatan * bil_Hasil_Jangka_Waktu / 3
+println(bil_Rekom_Harga)
+String hasil_RekomHarga = String.format ("%.0f", bil_Rekom_Harga)
+println(hasil_RekomHarga)
+
 def valRekomHarga = WebUI.getText(findTestObject("Page_Hitung Harga/Result_Rekomendasi Harga"))
-String hasil_RekomHarga = valRekomHarga.replaceAll("[^0-9]", "")
-WebUI.verifyEqual(hasil_RekomHarga, hasil_RekomHarga)
+String get_hasil_RekomHarga = valRekomHarga.replaceAll("[^0-9]", "")
+WebUI.verifyEqual(get_hasil_RekomHarga, hasil_RekomHarga)
 WebUI.takeScreenshot()
 
 
